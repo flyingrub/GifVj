cc02a8acd4c8e2dde2dd2faea28c2e52f0b01534
flyingrub <flyinggrub@gmail.com>
2016-10-31 14:20:03 +0100

    init

---
+++ gif/gifdl.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+"""gif dl
+
+Usage:
+    gifdl
+    gifdl <index>
+    gifdl -h | --help
+
+
+Options:
+    -h --help          Show this screen
+"""
+
+import logging
+import os
+import signal
+import sys
+import time
+import warnings
+import math
+import shutil
+import requests
+import re
+import tempfile
+import bs4 as BeautifulSoup
+import subprocess
+from docopt import docopt
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logging.getLogger('requests').setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.newline = print
+
+arguments = None
+index = None
+default_url = ('http://archillect.com/{0}')
+invalid_chars = '\/:*?|<>"'
+
+
+def main():
+    """
+    Main function, call parse_url
+    """
+    global index
+    arguments = docopt(__doc__, version='v0.0.1')
+    index = int(arguments['<index>']) if arguments['<index>'] else 1
+    signal.signal(signal.SIGINT, signal_handler)
+    logger.debug('Downloading to '+os.getcwd()+'...')
+
+    while index > 0:
+        archillect_request = requests.get(default_url.format(index))
+        soup = BeautifulSoup.BeautifulSoup(archillect_request.text, 'lxml')
+        gif = soup.find(id="ii")['src']
+
+        if gif.endswith('.gif'):
+            image_request = requests.get(gif, stream=True)
+            filename = "{0}.gif".format(index)
+            logger.debug(index)
+
+            img = image_request.content
+            with open(filename, 'wb') as out_file:
+                out_file.write(img)
+
+        index -= 1
+    end()
+
+
+def end():
+    with open("lastindex", 'w') as out_file:
+        out_file.write("{0}\n".format(index))
+
+
+def signal_handler(signal, frame):
+    """
+    Handle Keyboardinterrupt
+    """
+    logger.newline()
+    logger.info('Good bye!')
+    end()
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
---
+++ server.js
@@ -0,0 +1,38 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var url = require("url");
+var gifs;
+init();
+console.log('started')
+app.listen(1337);
+
+function init () {
+  fs.readdir('gif', function(err, items) { 
+    gifs = items.filter(function(item) {  return item.endsWith('.gif') });
+  });
+}
+
+function handler (req, res) {
+  var pathname = url.parse(req.url).pathname;
+  if (pathname == '/' || pathname == '/index.html') {
+    fs.readFile(__dirname + '/index.html', function(err, data) {
+      res.writeHead(200);
+      res.end(data);
+    });
+  } else {
+    fs.readFile(__dirname + pathname, function (err, data) {
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
+}
+
+io.on('connection', randomGif);
+
+function randomGif(socket) {
+  var index =  Math.floor((Math.random() * gifs.length - 1) + 0); 
+  socket.emit('gif', { 'gif': gifs[index] });
+  var randomTime = Math.floor((Math.random() * 7) + 2) * 1000; 
+  setTimeout(function() { randomGif(socket) }, randomTime);
+}
