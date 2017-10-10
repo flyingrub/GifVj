952ac8d38e1959730021266eabcb8a1b6bc32dea
flyingrub <flyingrub@protonmail.com>
2017-10-10 18:14:45 +0200

    cleanup

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
+io.on('connection', randomGif);
 var fs = require('fs');
 var url = require("url");
 var gifs;
 init();
-console.log('started')
+console.log('http://localhost:1337')
 app.listen(1337);
 
-function init () {
-  fs.readdir('gif', function(err, items) { 
+function init() {
+  fs.readdir('gif', function(err, items) {
     gifs = items.filter(function(item) {  return item.endsWith('.gif') });
   });
 }
 
-function handler (req, res) {
+function handler(req, res) {
   var pathname = url.parse(req.url).pathname;
   if (pathname == '/' || pathname == '/index.html') {
     fs.readFile(__dirname + '/index.html', function(err, data) {
@@ -28,11 +29,11 @@ function handler (req, res) {
   }
 }
 
-io.on('connection', randomGif);
 
 function randomGif(socket) {
-  var index =  Math.floor((Math.random() * gifs.length - 1) + 0); 
+  var index =  Math.floor((Math.random() * gifs.length - 1) + 0);
+  // console.log('test ' + index + '|' + gifs[index])
   socket.emit('gif', { 'gif': gifs[index] });
-  var randomTime = Math.floor((Math.random() * 6) + 2) * 1000; 
+  var randomTime = (Math.random() * 5 + 0.6 ) * 500;
   setTimeout(function() { randomGif(socket) }, randomTime);
 }
